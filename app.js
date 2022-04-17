3fba91e5880a866ff943f528ce7993df557127d4
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-04-17 18:12:40 +0900

    カスタムsassの導入

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +10,13 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 app.use(express.static('static'));
+app.use(sassMiddleware({
+    src: __dirname,
+    dest: path.join(__dirname, 'static'),
+    indentedSyntax: true,
+    debug: true,
+    outputStyle: 'compressed'
+}));
 
 app.listen('3000', () => {
     console.log('Application started');
